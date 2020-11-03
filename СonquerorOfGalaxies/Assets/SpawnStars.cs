dfa6dad7a86b11e8d80511ea461bfca8aa0923d3
ac0nite <a.sachuk@gmail.com>
2020-11-03 18:51:22 +0300

    create repo

---
+++ СonquerorOfGalaxies/Assets/SpawnStars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStars : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> _prefabsStars = null;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ СonquerorOfGalaxies/Assets/Sripts/MoveBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBase : MonoBehaviour
+{
+
+    [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private Vector2 _direction = Vector2.left;
+    [SerializeField] private float _speedRotation = 0.0f;
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.Translate(_direction * _speed * Time.deltaTime);
+        //transform.Rotate();
+    }
+}
