f2817c3543bd6667a1ce4159026e4207a0ae0f6d
ac0nite <a.sachuk@gmail.com>
2020-11-11 00:22:25 +0300

    ракета детектит ближайший объект и атакует

--- Space/Assets/Sripts/Ammunitions/Rocket.cs
+++ Space/Assets/Sripts/Ammunitions/Rocket.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] private float _damage = 0f;
+    [SerializeField] private int _damage = 0;
     [SerializeField] private HealthComponent _health = null;
     [SerializeField] private float _timeSearchEnemy = 1f;
-
+    [SerializeField] private MoveBase _move = null;
+    private Transform tr = null;
     void Start()
     {
-        StartCoroutine(AttackEnemy());
+        //StartCoroutine(AttackEnemy());
+        tr = GetComponent<RocketDetectEnemy>().Detect();
     }
 
     void Update()
@@ -18,6 +20,17 @@ public class Rocket : MonoBehaviour
         if (_health.Health == 0)
         {
             Destroy(this.gameObject);
+        }
+        if(tr == null) return;
+
+        if (Vector2.Distance(transform.position, tr.position) <= 0.2f)
+        {
+            tr.GetComponent<HealthComponent>().ChangeHealth(-_damage);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _move.Direction = (tr.position - transform.position).normalized;
         }
     }
 
--- Space/Assets/Sripts/Ammunitions/RocketDetectEnemy.cs
+++ Space/Assets/Sripts/Ammunitions/RocketDetectEnemy.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
 
 public class RocketDetectEnemy : MonoBehaviour
 {
 
-    private Collider2D[] _colliders = new Collider2D[3];
+    private Collider2D[] _colliders = new Collider2D[10];
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Detect();
     }
 
     // Update is called once per frame
@@ -18,8 +20,14 @@ public class RocketDetectEnemy : MonoBehaviour
         
     }
 
-    public void Detect()
+    public Transform Detect()
     {
-        Physics2D.OverlapCircleNonAlloc(transform.position, 7f, _colliders, LayerMask.GetMask("Enemy"));
+        Array.Clear(_colliders, 0, _colliders.Length);
+        var count = Physics2D.OverlapCircleNonAlloc(transform.position, 10f, _colliders, LayerMask.GetMask("Enemy"));
+        //Debug.Log($"Count: {count}");
+        var list = _colliders.ToList();
+        list.RemoveRange(count, list.Count-count);
+        list.Sort((a, b) => Vector2.Distance(transform.position, a.transform.position).CompareTo(Vector2.Distance(transform.position, b.transform.position)));
+        return list[0].transform;
     }
 }
